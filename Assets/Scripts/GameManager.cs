867f3479c9f1995ff54399d4b1728c36b486697a
Akram Gazali <akramburj@gmail.com>
2024-02-25 16:13:46 +0530

    hand tracking and game scene merged

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,7 +7,7 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
 
-    public float initialGameSpeed = 5f;
+    public float initialGameSpeed = 2f;
     public float gameSpeedIncrease = 0.1f;
     public float gameSpeed { get; private set; }
 
